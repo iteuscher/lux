b93026051003a337434ea840beea432784f4e482
Isaac Teuscher <isaacteuscher@Isaacs-MacBook-Pro.local>
2017-12-22 20:39:03 -0700

    encrypting passwords with salt and hashing

---
+++ lux-server/migrations/20171222172517_encrypt_user_password.js
@@ -0,0 +1,35 @@
+const { saltHashPassword } = require('../store');
+//pulling saltHashPassword function from store.js
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .table('users', t => {
+      t.string('salt').notNullable();
+      t.string('encrypted_password').notNullable();
+    })
+    .then(() => knex('users'))
+    .then(users => Promise.all(users.map(convertPassword)))
+    .then(() => {
+      return knex.schema.table('users', t => {
+        t.dropColumn('password')
+      })
+    })
+
+    function convertPassword (user) {
+      const { salt, hash } = saltHashPassword(user.password);
+      return knex('users')
+        .where({ id: user.id })
+        .update ({
+          salt,
+          encrypted_password: hash
+        })
+    }
+};
+
+exports.down = function(knex) {
+  return knex.schema.table('users', t => {
+    t.dropColumn('salt')
+    t.dropColumn('encrypted_password')
+    t.string('password').notNullable()
+  })
+};
--- lux-server/store.js
+++ lux-server/store.js
@@ -1,12 +1,33 @@
-var knex = require('knex')(require('./knexfile'));
+const crypto = require('crypto');
+//crypto doesn't need to be npm installed
+const knex = require('knex')(require('./knexfile'));
 module.exports = {
+  saltHashPassword,
   createUser ({ firstname, lastname, email, password }) {
-    console.log(`Added user: ${firstname} ${lastname} with email: ${email}`);
+    console.log(`Adding user: ${firstname} ${lastname} with email: ${email}`);
+    const { salt, hash } = saltHashPassword(password);
     return knex('users').insert({
+      salt,
+      encrypted_password: hash,
       firstname,
       lastname,
       email,
-      password
     });
   }
 };
+
+function saltHashPassword (password) {
+  let salt = getRandomString();
+  let hash = crypto
+  //hashing algorith sha512
+    .createHmac('sha512', salt)
+    .update(password)
+  return {
+    salt,
+    hash: hash.digest('hex')
+  }
+}
+
+function getRandomString () {
+  return crypto.randomBytes(4).toString('hex')
+}
